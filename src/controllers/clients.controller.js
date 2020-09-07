a23fe0779b27e8bdc7ebf38dac1468c87b6a1e39
gabrielblimoni <gabrielblimoni@gmail.com>
2020-09-07 12:31:57 -0300

    setting client routes

---
+++ src/controllers/clients.controller.js
@@ -0,0 +1,13 @@
+export default class ClientController {
+    create (req, res) {
+        res.send('Create Client')
+    }
+    
+    update (req, res) {
+        res.send('Update Client')
+    }
+    
+    list (req, res) {
+        res.send('List Client')
+    }
+}
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import ClientController from '../controllers/clients.controller.js'
+const clientController = new ClientController()
+
 const router = express.Router()
 
+// Testing route
 router.get('/', (req, res) => {
     res.send('Hello World')
 })
+
+// client routes
+router
+    .get('/clients', clientController.list)
+    .post('/clients', clientController.create)
+    .put('/clients', clientController.update)
 
 export default router
