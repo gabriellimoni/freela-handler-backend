62e25b329c92b726392cae5cd8e86efda726eed5
gabrielblimoni <gabrielblimoni@gmail.com>
2020-09-08 21:40:58 -0300

    clearing client controller

--- src/controllers/clients.controller.js
+++ src/controllers/clients.controller.js
@@ -3,32 +3,20 @@ const clientRepository = new ClientRepository()
 
 export default class ClientController {
     async create (req, res) {
-        try {
-            const client = req.body
-            const newClient = await clientRepository.create(client)
-            return res.status(201).json(newClient)
-        } catch (error) {
-            return res.status(400).send(error)
-        }
+        const client = req.body
+        const newClient = await clientRepository.create(client)
+        return res.status(201).json(newClient)
     }
     
     async update (req, res) {
-        try {
-            const clientId = req.params.id
-            const clientData = req.body
-            const updatedClient = await clientRepository.updateById(clientId, clientData)
-            return res.status(200).json(updatedClient)
-        } catch (error) {
-            return res.status(400).send(error)
-        }
+        const clientId = req.params.id
+        const clientData = req.body
+        const updatedClient = await clientRepository.updateById(clientId, clientData)
+        return res.status(200).json(updatedClient)
     }
     
     async list (req, res) {
-        try {
-            const clients = await clientRepository.listAll()
-            return res.status(200).json(clients)
-        } catch (error) {
-            return res.status(500).send(error)
-        }
+        const clients = await clientRepository.listAll()
+        return res.status(200).json(clients)
     }
 }
